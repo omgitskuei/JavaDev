288e57f960818c0f7f9ec35be9dafa249426cabd
Kuei-Feng Tung <kueifengtungchris@gmail.com>
2020-01-29 00:29:14 +0800

    2020 Jan 28 Update

--- src/development/ValidateMoney.java
+++ src/development/ValidateMoney.java
@@ -1,6 +1,7 @@
 package development;
 
 import java.util.ArrayList;
+import java.util.Currency;
 
 import util.CheckSubstring;
 import util.GetRuntimeInput;
@@ -11,17 +12,38 @@ public class ValidateMoney {
 		GetRuntimeInput util = new GetRuntimeInput();
 		String input = util.getConsoleInputBR("Enter Money Amount:");
 		
-		CheckSubstring util1 = new CheckSubstring();
+		float thisMoney = Float.parseFloat(input);
 		
-		ArrayList<String> delimitedThisMoney = util1.delimitAtDot(input);
-		
-		System.out.println("second half = "+delimitedThisMoney.get(1));
-		System.out.println("second half #digits =" +delimitedThisMoney.get(1).length());
-		
-		if (delimitedThisMoney.get(1).length() <= 2) {
-			System.out.println("good");
-		} else {
-			System.out.println("bad");
+		boolean result = validateMoney(thisMoney);
+		System.out.println("result: "+result);
+	}
+	
+	private static boolean validateMoney(float thisMoney) {
+		boolean success = false;
+		try {
+			System.out.println("BEGIN: ValidateMoney(float)");
+			// Get an instance of the local Currency
+			Currency localCurrency = Currency.getInstance("TWD");
+			// Get how many decimals are allowed for this Currency
+			int validCurrencyDecimals = localCurrency.getDefaultFractionDigits();  //equals =2 for TWD
+			// Check if the user input has at most this amount of decimals
+			String thisMoneyString = Float.toString(thisMoney);
+			// Get the length of the substring after the "."
+			CheckSubstring util = new CheckSubstring();
+			ArrayList<String> delimitedThisMoney = util.delimitAtDot(thisMoneyString);
+			
+			// If user entered a number with <= the number of digits allowed, success = TRUE
+			if (validCurrencyDecimals >= delimitedThisMoney.get(1).length()) {
+				System.out.println("VALID FLOAT");
+				success = true;
+			} else {
+				System.out.println("INVALID FLOAT");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
+		System.out.println("FINISH: validateMoney(float)");
+		return success;
 	}
 }
