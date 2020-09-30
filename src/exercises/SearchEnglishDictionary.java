2be1b7b370bc9b17e6e6f1a88960e225a0206eb7
Kuei <57666754+omgitskuei@users.noreply.github.com>
2020-09-30 17:59:35 +0800

    Sept 30 - added file read for wordbank

--- src/exercises/SearchEnglishDictionary.java
+++ src/exercises/SearchEnglishDictionary.java
@@ -1,10 +1,35 @@
 package exercises;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class SearchEnglishDictionary {
-	
+	/**
+	 * 
+	 * @param filePath
+	 * @return result
+	 */
+    public ArrayList<String> readWordbank(String filePath) {
+        File file = new File(filePath); 
+        
+        ArrayList<String> result = new ArrayList<String>();
+        try {
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                String word = sc.nextLine();
+                result.add(word);
+                System.out.println(word);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } 
+        return result;
+    }
+    
+    
 	public ArrayList<String> queryWholeWord(String wholeWord, HashMap<String, String> wordBank) {
 		ArrayList<String> result = new ArrayList<String>();
 		
@@ -22,17 +47,16 @@ public class SearchEnglishDictionary {
 		return result;
 	}
 	
-	public ArrayList<String> readWordbank(String a) {
-		
-		ArrayList<String> result = new ArrayList<String>();
-		return result;
-	}
+	
 
 	public static void main(String[] args) {
 		SearchEnglishDictionary a = new SearchEnglishDictionary();
+		String filePath = System.getProperty("user.dir") + "\\englishWordbank.txt";
+
+		// ArrayList<String> result = a.readWordbank(filePath);
 		
-		String input = "";
 		
+
 	}
 
 }
