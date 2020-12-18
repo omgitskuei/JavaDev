6be45ab22353974d0bdd6c2abc75ac1aff5d4d2f
董奎峰 <i92008cc63@cxlit.com.tw>
2020-12-18 09:31:25 +0800

    Dec 18

--- src/main/java/notes/dataTypes/UsingUUID.java
+++ src/main/java/notes/dataTypes/UsingUUID.java
@@ -21,27 +21,39 @@ public class UsingUUID {
         System.out.println(id);         // prints 00000000-0000-0000-0000-000000000000
         int variant = id.variant();
         int version = id.version();
-        System.out.println(variant);
-        System.out.println(version);
+        System.out.println("variant = " + variant);     // prints 0
+        System.out.println("version =" + version);      // prints 0
         
         UUID uuid = UUID.randomUUID();
         System.out.println(uuid);
-        int variant1 = id.variant();
-        int version1 = id.version();
-        System.out.println(variant1);
-        System.out.println(version1);
+        int variant1 = uuid.variant();
+        int version1 = uuid.version();
+        System.out.println("variant = " + variant1);    // prints 2
+        // The UUID v4 implementation uses random numbers as the source.
+        // The Java implementation is SecureRandom – which uses an unpredictable value as the seed to generate random numbers to reduce the chance of collisions.
+        System.out.println("version =" + version1);     // prints 4
         
         String uuidHexDigitString = "00000001-0000-0000-0000-00000000000";
         UUID uuid2 = UUID.fromString( uuidHexDigitString);
         System.out.println(uuid2);
-         
+        int variant2 = uuid2.variant();
+        int version2 = uuid2.version();
+        System.out.println("variant = " + variant2);    // prints 0
+        System.out.println("version =" + version2);     // prints 0
+        
         uuidHexDigitString = "00000001-0000-0000-0000-0000000000012345678910";      // Note: No errors even when too long!
         uuid2 = UUID.fromString( uuidHexDigitString);                               // returns 00000001-0000-0000-0000-012345678910  // it's been shortened
         System.out.println(uuid2);
+        variant2 = uuid2.variant();
+        version2 = uuid2.version();
+        System.out.println("variant = " + variant2);    // prints 0
+        System.out.println("version =" + version2);     // prints 0
         
-        uuidHexDigitString = "ABCDEFGH-IJKL-0000-0000-00000000000";                 // Letters are NOT ok
-        uuid2 = UUID.fromString( uuidHexDigitString);
+        uuidHexDigitString = "ABCDEFGH-IJKL-0000-0000-00000000000";                 // "ABCDEFGH" Letters are NOT valid, throws NumberFormatException
+        uuid2 = UUID.fromString( uuidHexDigitString);                               // it tried to Long.parseLong, Long.valueOf, Long.decode, all failed
         System.out.println(uuid2);
+        
+        
     }
 
 }
