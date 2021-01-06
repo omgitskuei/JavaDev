e429521fb7eda7fb72602e90425594701f8beb53
omgitskuei <kueifengtungchris@gmail.com>
2021-01-06 09:01:05 +0800

    Jan 6 - change java v bats

---
+++ SetJavaTo10.bat
@@ -0,0 +1,9 @@
+﻿@ECHO off
+ECHO Java 10.bat - Setting JAVA_HOME
+SET JAVA_HOME=C:\Program Files\Java\jdk-10.0.2
+ECHO setting PATH
+SET PATH=%JAVA_HOME%\bin;%PATH%
+ECHO Display java version
+java -version
+javac -version
+PAUSE
---
+++ SetJavaTo8.bat
@@ -0,0 +1,9 @@
+﻿@ECHO off
+ECHO Java 8.bat - Setting JAVA_HOME
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121
+ECHO setting PATH
+SET PATH=%JAVA_HOME%\bin;%PATH%
+ECHO Display java version
+java -version
+javac -version
+PAUSE
