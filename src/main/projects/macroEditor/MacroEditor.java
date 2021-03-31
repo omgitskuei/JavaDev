311455017905504246459e8eb9e3d54b8cc2bb16
omgitskuei <kueifengtungchris@gmail.com>
2021-03-31 22:56:21 +0800

    Mar 31 - Added init i18n

--- src/main/projects/macroEditor/MacroEditor.java
+++ src/main/projects/macroEditor/MacroEditor.java
@@ -11,106 +11,127 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.prefs.Preferences;
 
 public class MacroEditor {
 
-	private static final SimpleDateFormat TIMESTAMPFORMAT = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+	private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
 	private static final JFrame FRAME = new JFrame();
+	private static final Preferences PREFS = Preferences.userNodeForPackage(main.projects.macroEditor.MacroEditor.class);
 	private static ArrayList<String> fileContents = new ArrayList<String>();
+	private static HashMap<String, String> contribURLs = new HashMap<String, String>();
+	private static HashMap<String, HashMap<String, String>> i18nLabels = new HashMap<String, HashMap<String, String>>();
+	private static String version = "2021-03";
 	
 	private static String getDebugTimestamp() {
-		return TIMESTAMPFORMAT.format(new Date(System.currentTimeMillis()));
+		return TIMESTAMP_FORMAT.format(new Date(System.currentTimeMillis()));
 	}
 
-	
 	public static void main(String[] args) {
-		// initJFrame
 		
+		// initJFrame
 		FRAME.setTitle("MacroEditor");
 		FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		FRAME.setSize(400, 400);
 		FRAME.setMinimumSize(new Dimension(400, 300));
-
-		// preferences
-		final Preferences prefs = Preferences
-				.userNodeForPackage(main.projects.macroEditor.MacroEditor.class);
-		prefs.put("isDebug", "false");
+		
+		// initcontribURLs
+		contribURLs.put("Github", "https://github.com/omgitskuei");
+		contribURLs.put("Gitlab", "https://gitlab.com/omgitskuei");
+		
+		// initPreferences
+		PREFS.put("isDebug", "true");   // valid: true, false
+		PREFS.put("lang", "english");   // valid: english, chinese
+		
+		// initI18nLabels
+		HashMap<String, String> englishLabels = new HashMap<String, String>();
+		// menuBar labels
+		englishLabels.put("menuBarFile",        "File");
+		englishLabels.put("menuBarFileOpen",    "Open");
+		englishLabels.put("menuBarFileSaveAs",  "Save As");
+		englishLabels.put("menuBarHelp",        "Help");
+		englishLabels.put("menuBarHelpWelcome", "Welcome");
+		englishLabels.put("menuBarHelpAbout",   "About");
+		englishLabels.put("menuBarHelpContrib", "Contribute");
+		englishLabels.put("menuBarPrefs",       "Preferences");
+		englishLabels.put("menuBarPrefsDebug",  "Debug");
+		englishLabels.put("menuBarPrefsLang",   "Language");
+		englishLabels.put("menuBarPrefsLangEn", "English");
+		englishLabels.put("menuBarPrefsLangCn", "Chinese");
+		i18nLabels.put("english", englishLabels);
+		HashMap<String, String> chineseLabels = new HashMap<String, String>();
+		chineseLabels.put("menuBarFile",        "檔案");
+		chineseLabels.put("menuBarFileOpen",    "打開");
+		chineseLabels.put("menuBarFileSaveAs",  "儲存");
+		chineseLabels.put("menuBarHelp",        "Help-CN");
+		chineseLabels.put("menuBarHelpWelcome", "歡迎訊息");
+		chineseLabels.put("menuBarHelpAbout",   "關於App");
+		chineseLabels.put("menuBarHelpContrib", "Contribute-CN");
+		chineseLabels.put("menuBarPrefs",       "Preferences-CN");
+		chineseLabels.put("menuBarPrefsDebug",  "除錯");
+		chineseLabels.put("menuBarPrefsLang",   "語言");
+		chineseLabels.put("menuBarPrefsLangEn", "英文");
+		chineseLabels.put("menuBarPrefsLangCn", "中文");
+		i18nLabels.put("chinese", chineseLabels);
 
 		// initMenuBar
 		JMenuBar menuBar = new JMenuBar();
 		// - - - - - - - - - - - - - - - Menu bar > File - - - - - - - - - - - - - - -
-		JMenu menu1_file = new JMenu("File");
+		JMenu menuBarFile = new JMenu(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarFile"));
 		// - - - - - - - - - - - - - - - Menu bar > File > Open - - - - - - - - - - - -
-		JMenuItem menu1_file_open = new JMenuItem("Open");
-		menu1_file_open.addActionListener(new ActionListener() {
+		JMenuItem menuBarFileOpen = new JMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarFileOpen"));
+		menuBarFileOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actEvent) {
-				String isDebug = prefs.get("isDebug", "false");
-				if (isDebug.equals("true")) {
-					System.out.println("[" + getDebugTimestamp() + "] Clicked File -> " + actEvent.getActionCommand());
-				}			
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked File -> " + actEvent.getActionCommand());
 				JFileChooser fileChooser = new JFileChooser("src\\main\\projects\\macroEditor\\");
 				int chosenFile = fileChooser.showOpenDialog(FRAME);
 				if (chosenFile == JFileChooser.APPROVE_OPTION) { // JFileChooser.APPROVE_OPTION is 0
 					File file = fileChooser.getSelectedFile();
-					if (isDebug.equals("true")) {
-						System.out.println("[" + getDebugTimestamp() + "] User selected a file;");
-						System.out.println("File name:     " + file.getName());
-						System.out.println("Absolute path: " + file.getAbsolutePath());
-						System.out.println("Writeable:     " + file.canWrite());
-						System.out.println("Readable       " + file.canRead());
-						System.out.println("Size (bytes):  " + file.length());
-					}
+					outputDebugMsg("[" + getDebugTimestamp() + "] User selected a file;");
+					outputDebugMsg("File name:     " + file.getName());
+					outputDebugMsg("Absolute path: " + file.getAbsolutePath());
+					outputDebugMsg("Writeable:     " + file.canWrite());
+					outputDebugMsg("Readable       " + file.canRead());
+					outputDebugMsg("Size (bytes):  " + file.length());
 					try {
 						Scanner myReader = new Scanner(file);
-						if (isDebug.equals("true")) {
-							System.out.println("<<File contents - BEGIN>>");
-						}
+						outputDebugMsg("<<File contents - BEGIN>>");
 						int lnCount = 1;
 						while (myReader.hasNextLine()) {
 							String eachLine = myReader.nextLine();
-							if (isDebug.equals("true")) {
-								System.out.println("(ln"+lnCount+") "+eachLine);
-							}
+							outputDebugMsg("(ln"+lnCount+") "+eachLine);
 							fileContents.add(eachLine);
 							lnCount++;
 						}
-						if (isDebug.equals("true")) {
-							System.out.println("<<File contents - END>>");
-						}
+						outputDebugMsg("<<File contents - END>>");
 						myReader.close();
 					} catch (FileNotFoundException e) {
-						System.out.println("[" + getDebugTimestamp() + "] A FileNotFoundException error occurred.");
+						System.err.println("[" + getDebugTimestamp() + "] A FileNotFoundException error occurred.");
 						e.printStackTrace();
 					}
 				} else {
-					if (isDebug.equals("true")) {
-						System.out.println("[" + getDebugTimestamp() + "] User didn't select file");
-					}
+					outputDebugMsg("[" + getDebugTimestamp() + "] User didn't select file");
 				}
 			}
 		});
-		menu1_file.add(menu1_file_open);
+		menuBarFile.add(menuBarFileOpen);
 		// - - - - - - - - - - - - - - - Menu bar > File > Save as - - - - - - - - - - -
-		JMenuItem menu1_file_saveAs = new JMenuItem("Save as");
-		menu1_file_saveAs.addActionListener(new ActionListener() {
+		JMenuItem menuBarFileSaveAs = new JMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarFileSaveAs"));
+		menuBarFileSaveAs.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actEvent) {
-				String isDebug = prefs.get("isDebug", "false");
-				if (isDebug.equals("true")) {
-					System.out.println("[" + getDebugTimestamp() + "] Clicked File -> " + actEvent.getActionCommand());
-				}
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked File -> " + actEvent.getActionCommand());
 				JFileChooser fileChooser = new JFileChooser("src\\main\\projects\\macroEditor\\");
 				fileChooser.setDialogTitle("Save As");   
 				int userSelection = fileChooser.showSaveDialog(FRAME);
 				if (userSelection == JFileChooser.APPROVE_OPTION) {
 				    File fileToSave = fileChooser.getSelectedFile();
-				    if (isDebug.equals("true")) {
-				    	System.out.println("[" + getDebugTimestamp() + "] Save as file name: " + fileToSave.getName());
-				    	System.out.println("[" + getDebugTimestamp() + "] Save as file:      " + fileToSave.getAbsolutePath());
-				    }
+				    outputDebugMsg("[" + getDebugTimestamp() + "] Save as file name: " + fileToSave.getName());
+				    outputDebugMsg("[" + getDebugTimestamp() + "] Save as file:      " + fileToSave.getAbsolutePath());
 				    FileWriter myWriter;
 					BufferedWriter writer;
 					try {
@@ -118,87 +139,149 @@ public class MacroEditor {
 						writer = new BufferedWriter(myWriter);
 						for(int index = 0; index<fileContents.size(); index++) {
 							String eachLine = fileContents.get(index);
-							if (isDebug.equals("true")) {
-								System.out.println("[" + getDebugTimestamp() + "] On (Ln"+index+1+"), wrote \"" + eachLine + "\"");
-							}
+							outputDebugMsg("[" + getDebugTimestamp() + "] On (Ln"+(index+1)+"), wrote \"" + eachLine + "\"");
 							eachLine = eachLine + "\r\n";
 							writer.write(eachLine);
 						}
 						writer.close();
 					} catch (IOException e) {
-						System.out.println("[" + getDebugTimestamp() + "] An IOException error occurred.");
+						System.err.println("[" + getDebugTimestamp() + "] An IOException error occurred.");
 						e.printStackTrace();
 					}
 				}
 			}
 		});
-		menu1_file.add(menu1_file_saveAs);
-		menuBar.add(menu1_file);
+		menuBarFile.add(menuBarFileSaveAs);
+		menuBar.add(menuBarFile);
 		// - - - - - - - - - - - - - - - Menu bar > Help - - - - - - - - - - - - - - -
-		JMenu menu2_help = new JMenu("Help");
+		JMenu menuBarHelp = new JMenu(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarHelp"));
 		// - - - - - - - - - - - - - - - Menu bar > Help > Welcome - - - - - - - - - - -
-		JMenuItem menu2_help_welcome = new JMenuItem("Welcome");
-		menu2_help_welcome.addActionListener(new ActionListener() {
+		JMenuItem menuBarHelpWelcome = new JMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarHelpWelcome"));
+		menuBarHelpWelcome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actEvent) {
-				String isDebug = prefs.get("isDebug", "false");
-				if (isDebug.equals("true")) {
-					System.out.println("[" + getDebugTimestamp() + "] Clicked Help -> " + actEvent.getActionCommand());
-				}
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked Help -> " + actEvent.getActionCommand());
 			}
 		});
-		menu2_help.add(menu2_help_welcome);
+		menuBarHelp.add(menuBarHelpWelcome);
 		// - - - - - - - - - - - - - - - Menu bar > Help > About - - - - - - - - - - - -
-		JMenuItem menu2_help_about = new JMenuItem("About");
-		menu2_help_about.addActionListener(new ActionListener() {
+		JMenuItem menuBarHelpAbout = new JMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarHelpAbout"));
+		menuBarHelpAbout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actEvent) {
-				String isDebug = prefs.get("isDebug", "false");
-				if (isDebug.equals("true")) {
-					System.out.println("[" + getDebugTimestamp() + "] Clicked Help -> " + actEvent.getActionCommand());
-				}
-				PopupFactory pf = PopupFactory.getSharedInstance();
-				JFrame f = new JFrame();
-				JPanel p = new JPanel();
-				JTextField t = new JTextField(10);
-                p.setSize(t.getWidth(), t.getHeight());
-                p.setPreferredSize(new Dimension(t.getWidth(), t.getHeight()));
-                Point l = t.getLocationOnScreen();
-                Popup popup = pf.getPopup(f, p, l.x, l.y + t.getHeight());
-                popup.show();
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked Help -> " + actEvent.getActionCommand());
+				outputDebugMsg("[" + getDebugTimestamp() + "] version = " + version);
+				JOptionPane.showMessageDialog(
+					FRAME, 
+					"MacroEditor (Version:" + version + ")\r\n"
+					+ "\r\n"
+					+ "MacroEditor was created by Kuei-Feng Tung in 2021 \r\n"
+					+ "with the purpose of simplifying the creation of \r\n"
+					+ "automation commands. With this app, users can \r\n"
+					+ "automate using text files, without needing the \r\n"
+					+ "know-how of coding with Java robot API. \r\n"
+					+ "\r\n"
+					+ "(C) Copyright MacroEditor 2021. All rights reserved.\r\n"
+					+ "MacroEditor and the MacroEditor logo are trademarks \r\n"
+					+ "of Kuei-Feng Tung, https://github.com/omgitskuei. \r\n"
+					+ "The MacroEditor logo cannot be altered without \r\n"
+					+ "permission. Oracle and Java are trademarks or \r\n"
+					+ "registered trademarks of Oracle and/or its \r\n"
+					+ "affiliates.",
+					"About MacroEditor", 
+					JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
-		menu2_help.add(menu2_help_about);
+		menuBarHelp.add(menuBarHelpAbout);
 		// - - - - - - - - - - - - - - - Menu bar > Help > Contribute - - - - - - - - -
-		JMenuItem menu2_help_contribute = new JMenuItem("Contribute");
-		menu2_help_contribute.addActionListener(new ActionListener() {
+		JMenuItem menuBarHelpContrib = new JMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarHelpContrib"));
+		menuBarHelpContrib.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actEvent) {
-				String isDebug = prefs.get("isDebug", "false");
-				if (isDebug.equals("true")) {
-					System.out.println("[" + getDebugTimestamp() + "] Clicked Help -> " + actEvent.getActionCommand());
-				}
-			}
-		});
-		menu2_help.add(menu2_help_contribute);
-		menuBar.add(menu2_help);
-		// - - - - - - - - - - - - - - - Menu bar > Preferences - - - - - - - - - - - -
-		JMenu menu3_pref = new JMenu("Preferences");
-		// - - - - - - - - - - - - - - - Menu bar > Preferences > Debug - - - - - - - -
-		JCheckBoxMenuItem menu3_pref_debug = new JCheckBoxMenuItem("Debug");
-		menu3_pref_debug.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent actEvent) {
-				String isDebug = prefs.get("isDebug", "false");
-				if (isDebug.equals("false")) {
-					prefs.put("isDebug", "true");
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked Help -> " + actEvent.getActionCommand());
+				outputDebugMsg("[" + getDebugTimestamp() + "] contribURLs = " + contribURLs);
+				String selectedOption = (String) JOptionPane.showInputDialog(
+					FRAME,
+					"Contribute by following me on:", 
+					"Contribute",					// Dialog window title
+					JOptionPane.PLAIN_MESSAGE, 		// Type of Dialog window
+					null, 							// Icon
+					contribURLs.keySet().toArray(), // Select options (Object[]), convert Map to Set<String> to String[]
+					"Github"						// Default option
+				);
+				// If the user presses OK, and return a selected option, go to the corresponding URL
+				if ((selectedOption != null) && (selectedOption.length() > 0)) {
+					outputDebugMsg("[" + getDebugTimestamp() + "] User chose [" + selectedOption + "]");
+					try {
+						java.awt.Desktop.getDesktop().browse(java.net.URI.create(contribURLs.get(selectedOption)));
+					} catch (IOException ioE) {
+						System.err.println("[" + getDebugTimestamp() + "] A IOException error occurred.");
+						ioE.printStackTrace();
+					}
 				} else {
-					prefs.put("isDebug", "false");
+					outputDebugMsg("[" + getDebugTimestamp() + "] User closed Contribute dialog window.");
 				}
-				if (isDebug.equals("true")) {
-					System.out.println(
-							"[" + getDebugTimestamp() + "] Clicked Preferences -> " + actEvent.getActionCommand());
-				}
+				// Close dialog window, Return to FRAME
+				return;
 			}
 		});
-		menu3_pref.add(menu3_pref_debug);
-		menuBar.add(menu3_pref);
+		menuBarHelp.add(menuBarHelpContrib);
+		menuBar.add(menuBarHelp);
+		// - - - - - - - - - - - - - - - Menu bar > Preferences - - - - - - - - - - - -
+		JMenu menuBarPrefs = new JMenu(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefs"));
+		// - - - - - - - - - - - - - - - Menu bar > Preferences > Debug - - - - - - - -
+		JCheckBoxMenuItem menuBarPrefsDebug;
+		if(PREFS.get("isDebug", "false").equals("true")) {
+			menuBarPrefsDebug = new JCheckBoxMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsDebug"), true); 
+		} else {
+			menuBarPrefsDebug = new JCheckBoxMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsDebug")); 
+		}
+		menuBarPrefsDebug.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actEvent) {
+				if (PREFS.get("isDebug", "false").equals("false")) {
+					PREFS.put("isDebug", "true");
+				} else {
+					PREFS.put("isDebug", "false");
+				}
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked Preferences -> " + actEvent.getActionCommand());
+			}
+		});
+		menuBarPrefs.add(menuBarPrefsDebug);
+		// - - - - - - - - - - - - - - - Menu bar > Preferences > Language - - - - - - - -
+		JMenu menuBarPrefsLang = new JMenu(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLang"));
+		ButtonGroup langRadioGrp = new ButtonGroup();
+		
+		// - - - - - - - - - - - - - - - Menu bar > Preferences > Language > English - - - - - - - -
+		JRadioButtonMenuItem menuBarPrefsLangEn;
+		if(PREFS.get("lang", "english").equals("english")) {
+			menuBarPrefsLangEn = new JRadioButtonMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLangEn"), true); 
+		} else {
+			menuBarPrefsLangEn = new JRadioButtonMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLangEn")); 
+		}
+		menuBarPrefsLangEn.setActionCommand(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLangEn"));
+		menuBarPrefsLangEn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actEvent) {
+				String command = langRadioGrp.getSelection().getActionCommand();
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked Preferences -> Language -> " + command);
+			}
+		});
+		menuBarPrefsLang.add(menuBarPrefsLangEn);
+		langRadioGrp.add(menuBarPrefsLangEn);
+		// - - - - - - - - - - - - - - - Menu bar > Preferences > Language > Chinese - - - - - - - -
+		JRadioButtonMenuItem menuBarPrefsLangCn;
+		if(PREFS.get("lang", "english").equals("chinese")) {
+			menuBarPrefsLangCn = new JRadioButtonMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLangCn"), true); 
+		} else {
+			menuBarPrefsLangCn = new JRadioButtonMenuItem(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLangCn")); 
+		}
+		menuBarPrefsLangCn.setActionCommand(i18nLabels.get(PREFS.get("lang", "english")).get("menuBarPrefsLangCn"));
+		menuBarPrefsLangCn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actEvent) {
+				String command = langRadioGrp.getSelection().getActionCommand();
+				outputDebugMsg("[" + getDebugTimestamp() + "] Clicked Preferences -> Language -> " + command);
+			}
+		});
+		menuBarPrefsLang.add(menuBarPrefsLangCn); 
+		langRadioGrp.add(menuBarPrefsLangCn);
+		menuBarPrefs.add(menuBarPrefsLang);
+		menuBar.add(menuBarPrefs);
 		// InitMenuBarJPanel
 		JPanel menuBarPanel = new JPanel();
 		menuBarPanel.setLayout(new GridBagLayout());
@@ -271,7 +354,20 @@ public class MacroEditor {
 		FRAME.getContentPane().add(BorderLayout.CENTER, new JButton("CENTER"));
 		FRAME.getContentPane().add(BorderLayout.EAST, new JButton("EAST"));
 
-		FRAME.setVisible(true);
+		SwingUtilities.invokeLater(() -> FRAME.setVisible(true));
 	}
 
+	// Helper methods
+	
+	private void updateLang(String newLang) {
+		outputDebugMsg("[" + getDebugTimestamp() + "] Update labels to Language [" + newLang + "]");
+		HashMap<String, String> newLabels = i18nLabels.get(newLang);
+		
+	}
+	
+	private static void outputDebugMsg(String message) {
+		if(PREFS.get("isDebug", "false").equals("true")) {
+			System.out.println(message);
+		}
+	}
 }
