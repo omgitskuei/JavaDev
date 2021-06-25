cc3113f010b3babcfde9f164c22a2d19becc6f8c
omgitskuei <kueifengtungchris@gmail.com>
2021-06-26 00:59:21 +0800

    TimeElapse V2 bat file for runnable jar

---
+++ src/main/projects/timeElapse/version2/Run_TimeElapseV2.bat
@@ -0,0 +1,18 @@
+@ECHO OFF 
+REM This bat file runs the runnable jar TimeElapseV1.jar - note this bat file has to be in the same directory as the runnable jar to work.
+ECHO ============================================================
+ECHO TimeElapse V.2
+ECHO --------------
+ECHO TimeElapse (Version:2) was created by Kuei-Feng Tung in 2021
+ECHO for calculating time differences. 
+ECHO With this app, users can quickly find time elapsed between 
+ECHO two given times within a day.
+ECHO (C) Copyright TimeElapse 2021. All rights reserved.
+ECHO TimeElapse and the TimeElapse logo are trademarks of 
+ECHO Kuei-Feng Tung, https://github.com/omgitskuei.
+ECHO The TimeElapse logo cannot be altered without permission.
+ECHO Oracle and Java are trademarks or registered trademarks 
+ECHO of Oracle and/or its affiliates.
+ECHO ============================================================
+java -jar TimeElapseV2.jar
+::PAUSE
