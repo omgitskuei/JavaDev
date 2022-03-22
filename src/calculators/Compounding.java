86fb49737c013ca154736ac43b9a1fcbde5d3a15
omgitskuei <kueifengtungchris@gmail.com>
2022-03-23 00:20:05 +0800

    Create Compounding.java

---
+++ src/calculators/Compounding.java
@@ -0,0 +1,18 @@
+package calculators;
+
+public class Compounding {
+	
+	static double pv = 5000.0;
+	static int t = 2;
+	static double r = 0.03;
+	
+	public static void main(String[] args) {
+		System.out.println("FV = PV * (1 + r)^t");
+		System.out.println("FV = " + String.valueOf(pv) + " * " + "(1 + " + String.valueOf(r) + ")^" + String.valueOf(t));
+		System.out.println("FV = " + String.valueOf(pv) + " * " + "(" + String.valueOf((1+r)) + ")^" + String.valueOf(t));
+		System.out.println("FV = " + String.valueOf(pv) + " * " + String.valueOf(Math.pow((1+r), t)));
+		System.out.println("FV = " + String.valueOf(pv*Math.pow((1+r), t)));
+
+	}
+
+}
