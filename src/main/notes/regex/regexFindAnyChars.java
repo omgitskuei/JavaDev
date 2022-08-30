7e06fd294f145b5ee8b94d06e7fcf1717d020fbb
omgitskuei <kueifengtungchris@gmail.com>
2022-08-30 09:27:19 +0800

    Create regexFindAnyChars.java

---
+++ src/main/notes/regex/regexFindAnyChars.java
@@ -0,0 +1,26 @@
+package main.notes.regex;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regexFindAnyChars {
+
+	/*
+	 * Notice the example String has a Chinese character.
+	 * To pattern match while allowing anything,
+	 * use .{min number of any chars, maximum number of any chars}
+	 */
+	public static void main(String[] args) {
+		
+		String exampleStr = "P1P 連ASD";
+		
+		Pattern pattern = Pattern.compile("[A-Z0-9]{1,3} .{1,20}");
+		Matcher matcher = pattern.matcher(exampleStr);
+		boolean matchFound = matcher.matches();
+		System.out.println(matchFound);
+		// true
+
+	}
+
+}
